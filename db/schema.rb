a92fb761f3929b8181278cbc966715c6fb73fa1a
ZAKI <rq-Bohemian1506-at-321899771753>
2024-09-07 11:03:53 +0900

    課題10画像アップローダー設置

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_03_015234) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_07_011544) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
